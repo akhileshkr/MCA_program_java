a6680e749381f0fa8d6db031aba5e5236b95b994
akhileshkr <akhilesh0614@gmail.com>
2017-10-29 23:59:44 +0530

    updated

--- butten.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-public class butten extends JApplet implements ActionListener{
-JTextField tf;
-public void init()
-{
-Container contentPane=getContentPane();
-contentPane.setLayout(new
